03bff1912643dd816fabe62cabc08128f1203864
onurulusoy <ulusoyonur@yahoo.com>
2017-02-02 09:58:21 +0200

    Update runMe.bat

--- runMe.bat
+++ runMe.bat
@@ -1,3 +1,3 @@
 @echo off
-node node_modules\cucumber\bin\cucumber.js features\mainPage.feature -f json:test\report\results.json
+node node_modules\cucumber\bin\cucumber.js -r features\ -f json:test\report\results.json
 pause
