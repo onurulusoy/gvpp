4dd8f721a1981c1e24d83fd2045d90ace9813442
onurulusoy <ulusoyonur@yahoo.com>
2017-02-07 17:05:51 +0200

    Update huntGroup.js

--- features/step_definitions/huntGroup.js
+++ features/step_definitions/huntGroup.js
@@ -18,6 +18,10 @@ module.exports = function () {
             return element.click();
         });
     });
+    
+    this.Then('pause', function () {
+        return this.driver.wait(5000);
+    });
 
     this.Then('I see Groups title', function () {
         var xpath = "//*[@id='smb-manage-content']/div/div/div/div[2]/div[1]/div[2]/div/h3/span/span[1]";
